36a4842b302c9afbf2b21b41ff03230ae945b459
Nicolás Vera <nicolasverapalominos@gmail.com>
2018-04-23 18:07:35 -0300

    crud listo con componentes

--- app.js
+++ app.js
@@ -1,22 +1,76 @@
+Vue.component('icon',{
+	template: '<span :class="cssClasses" aria-hidden="true"></span>',
+	props: ['draw'],
+	computed: {
+		cssClasses: function(){
+			return 'glyphicon glyphicon-' + this.draw;
+		}
+	}
+});
+
+Vue.component('list',{
+	template: '#task-list',
+	props: ['task','index','tasks','new_task'],
+	methods: {
+		toggleStatus: function(){
+			this.task.pending = !this.task.pending;
+			this.task.editing = false
+		},
+		editTask: function(){
+			this.task.editing = true,
+			this.task.draft = this.task.description
+		},
+		updateTask: function(){
+			this.task.description = this.task.description,
+			this.task.editing = false,
+			this.task.draft = ''
+		},
+		cancelTask: function(){
+			this.task.editing = false,
+			this.task.description = this.task.draft,
+			this.task.draft = ''
+		},
+		deleteTask: function(index){
+			this.tasks.splice(this.index, 1);
+		}		
+	}
+});
+
 var vm = new Vue({
 	el: '#root',
 	data: {
+		new_task: '',
 		tasks: [
 			{
-				description: 'Listar tareas',
+				description: 'Tarea 1',
 				pending: true,
-				editing: false
+				editing: false,
+				draft: ''
 			},
 			{
-				description: 'Crear tareas',
+				description: 'Tarea 2',
 				pending: true,
-				editing: true
+				editing: true,
+				draft: ''
 			},
 			{
-				description: 'Editar y Eliminar tareas',
+				description: 'Tarea 3',
 				pending: false,
-				editing: false
+				editing: false,
+				draft: ''
 			}
 		]
+	},
+	methods: {
+		createTask: function(){
+			if(this.new_task !== ''){
+				this.tasks.push({
+					description: this.new_task,
+					pending: true,
+					editing: false
+				});
+				this.new_task = '';
+			}
+		}		
 	}
 });
